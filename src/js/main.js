6041433633491c12ff978d39bc94ac435201cb1b
Joseph Soria <josephsoria6@gmail.com>
2022-07-17 17:30:45 -0500

    Wrote function to hide/display answers

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,23 @@
 let resizeTimeoutId;
 
 window.addEventListener("resize", () => {
-  clearTimeout(resizeTimeoutId);  
+  clearTimeout(resizeTimeoutId);
   $(".sun").removeClass("animate-sun");
   resizeTimeoutId = setTimeout(() => {
     $(".sun").addClass("animate-sun");
-  }, 500)
-})
+  }, 500);
+});
+
+$(".question-box").click(({ target }) => {
+  let questionEl = $(target).is("div")
+    ? target
+    : target.parentNode;
+  const questionNumber = questionEl.dataset.question;
+  const selectedAnswer = $(`.answer-box[data-question=${questionNumber}]`)
+  if (!selectedAnswer.hasClass("close")) {
+    $(`.answer-box[data-question=${questionNumber}]`).addClass("close");
+    return;
+  }
+  $(".answer-box").addClass("close");
+  selectedAnswer.removeClass("close");
+});
