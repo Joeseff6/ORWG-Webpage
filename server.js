6037fd4cdb78ffda54ba923a8a74219e048c54df
Joseph Soria <josephsoria6@gmail.com>
2022-07-17 22:44:15 -0500

    Wrote static middleware, imported morgan

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require("express")
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+
 const app = express();
 const port = 4000;
+
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send(`Port ${port} is listening`)
