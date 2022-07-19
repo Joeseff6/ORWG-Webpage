9fd2e0ef0d23a31581cd70712cf36a4e8a4aa309
Joseph Soria <josephsoria6@gmail.com>
2022-07-19 00:22:04 -0500

    Added middleware to use routing

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const routes = require("./routes");
 
 const app = express();
 const port = 4000;
 
+app.use(express.json());
 app.use(logger("combined"));
 app.use(express.static(path.join(__dirname, "client", "public")));
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
-
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "public", "html", "login.html"));
 });
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
