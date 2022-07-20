5e0f70983880af00cc01fdbb0c07e6503f9e1f4a
Joseph Soria <josephsoria6@gmail.com>
2022-07-20 10:00:50 -0500

    Moved /login endpoint, imported db and bcrypt

--- routes/api/adminRoutes.js
+++ routes/api/adminRoutes.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
+const db = require("../../models");
+const bcrypt = require("bcrypt");
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "public", "html", "login.html"));
+});
 
 module.exports = router;
