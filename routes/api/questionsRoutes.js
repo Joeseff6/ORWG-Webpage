6ead16a316c95c4839a90948d262a9e8f1c61598
Joseph Soria <josephsoria6@gmail.com>
2022-07-20 19:38:24 -0500

    Created get method for api/questions/

--- routes/api/questionsRoutes.js
+++ routes/api/questionsRoutes.js
@@ -1,4 +1,9 @@
 const router = require("express").Router();
+const db = require("../../models"); 
 
+router.get("/", async(req, res) => {
+  const questions = await db.Questions.find({});
+  res.status(200).json(questions);
+})
 
 module.exports = router;
