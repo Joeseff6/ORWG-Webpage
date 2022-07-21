976a7893fe330a96de5d6f5ae70da9933cf1314c
Joseph Soria <josephsoria6@gmail.com>
2022-07-20 19:17:19 -0500

    Created and imported api/questions route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const adminRoutes = require("./adminRoutes");
+const questionsRoutes = require("./questionsRoutes");
 
 router.use("/admin", adminRoutes);
+router.use("/questions", questionsRoutes);
+
 
 module.exports = router;
