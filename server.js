2b42c0e6ff9b4f410afc4ca9885048b409eafdc0
Joseph Soria <josephsoria6@gmail.com>
2022-07-22 01:17:25 -0500

    Wrote function to hash admin password

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+const bcrypt = require("bcrypt");
 const routes = require("./routes");
 const db = require("./models");
 
@@ -10,17 +11,42 @@ const PORT = 4000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(logger("combined"));
+app.use(express.static(path.join(__dirname, "client", "public")));
+
 app.get("/", async (req, res) => {
   try {
-    // console.log(await db.Admin.findById("askdfh"))
     res.sendFile(path.join(__dirname, "client", "public", "index.html"));
   } catch (err) {
     console.log(err.message);
   }
 });
 
-app.use(express.static(path.join(__dirname, "client", "public")));
+async function initAdminPassword() {
+  try {
+    let admin = await db.Admin.findOne();
+    if (admin.adminPassword) {
+      return;
+    } else {
+      const saltRounds = 10;
+      let adminPassword = process.env.ADMIN_PASSWORD;
+      bcrypt.hash(adminPassword, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Password hashed!");
+          await db.Admin.updateOne({ adminUsername: process.env.ADMIN_USERNAME}, 
+            {$set: {adminPassword: hash}}
+          )
+          console.log("Password updated for admin.");
+        }
+      })
+    }
+  } catch(err) {
+    console.log(err.message);
+  }
+}
+
+initAdminPassword();
 
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "public", "html", "login.html"));
