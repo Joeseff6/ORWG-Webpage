5ee584e85c90068267ab2788fdb136004453e1d7
Joseph Soria <josephsoria6@gmail.com>
2022-07-24 23:55:36 -0500

    Began writing "/" post request function

--- routes/api/adminRoutes.js
+++ routes/api/adminRoutes.js
@@ -2,4 +2,21 @@ const router = require("express").Router();
 const db = require("../../models");
 const bcrypt = require("bcryptjs");
 
+router.post("/", async(req, res) => {
+  console.log(req.body)
+  try {
+    const { adminPassword } = await db.Admin.findOne({adminUsername: req.body.adminAttempt});
+    bcrypt.compare(req.body.passwordAttempt, adminPassword, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const validationMessage = res ? "Validation success!" : "Wrong password";
+        console.log(validationMessage);
+      }
+    });
+  } catch (err) {
+    console.log(err.message)
+  }
+})
+
 module.exports = router;
