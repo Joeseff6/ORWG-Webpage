942981b56c0e8a17474ded04d5a054c6906dc1b4
Joseph Soria <josephsoria6@gmail.com>
2022-07-27 21:59:19 -0500

    Updated form submit function

--- client/public/js/login.js
+++ client/public/js/login.js
@@ -1,21 +1,19 @@
-function onFormSubmit() {
-  $("form").getElementById("login-form").submit(async (e) => {
-    e.preventDefault();
-    const adminAttempt = $("#username").val();
-    const passwordAttempt = $("#password").val();
-    try {
-        await $.ajax({
-        url: "/api/admin",
-        method: "POST",
-        data: {
-          adminAttempt,
-          passwordAttempt,
-        }
-      })
-      document.location.replace("/");
-    } catch({ responseJSON }) {
-      $(".error-message").text(responseJSON.message);
-      $(".error-box").removeClass("hide");
-    }
-  })
-}
+document.getElementById("login-form").addEventListener("submit", async(e) => {
+  e.preventDefault();
+  const adminAttempt = $("#username").val();
+  const passwordAttempt = $("#password").val();
+  try {
+      await $.ajax({
+      url: "/api/admin",
+      method: "POST",
+      data: {
+        adminAttempt,
+        passwordAttempt,
+      }
+    })
+    document.location.replace("/");
+  } catch({ responseJSON }) {
+    $(".error-message").text(responseJSON.message);
+    $(".error-box").removeClass("hide");
+  }
+})
