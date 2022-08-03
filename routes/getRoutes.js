bfe9844e7f8ea673a5db6d474483844e854ce909
Joseph Soria <josephsoria6@gmail.com>
2022-08-02 21:59:09 -0500

    Wrote endpoint GET method for question edit

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const isLoggedIn = require("../utils/isLoggedIn");
 const isNotLogged = require("../utils/isNotLogged");
+const db = require("../models/index");
 
 router.get("/", isLoggedIn, async (req, res) => {
   try {
@@ -22,5 +23,15 @@ router.get("/admin", isNotLogged, async (req, res) => {
   }
 });
 
+router.get("/admin/:id", isNotLogged, async (req, res) => {
+  try {
+    console.log(req.params)
+    let question = await db.Questions.findById(req.params.id);
+    console.log(question);
+    res.render("partials/addQuestion");
+} catch (err) {
+    console.log(err.message);
+  }
+});
 
 module.exports = router;
