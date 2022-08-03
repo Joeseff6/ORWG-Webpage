ee84796318b328b176c7a4906d7f1e913c6f34fc
Joseph Soria <josephsoria6@gmail.com>
2022-08-02 23:28:31 -0500

    Wrote method for PUT method to update

--- routes/api/questionsRoutes.js
+++ routes/api/questionsRoutes.js
@@ -6,4 +6,16 @@ router.get("/", async(req, res) => {
   res.status(200).json(questions);
 })
 
+router.put("/:id", async(req, res) => {
+  try {
+    await db.Questions.findByIdAndUpdate(req.params.id, {
+      question: req.body.question,
+      answer: req.body.answer,
+    })
+    res.status(200).json({ message: "Question and answer updated!"});
+  } catch(err) {
+    console.log(err.message);
+  }
+})
+
 module.exports = router;
