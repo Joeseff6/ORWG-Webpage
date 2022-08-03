f08d9fed8d3068259973fab098255b86b2fc9b36
Joseph Soria <josephsoria6@gmail.com>
2022-08-02 23:28:01 -0500

    Wrote function to submit form content

---
+++ client/public/js/edit.js
@@ -0,0 +1,32 @@
+document.getElementById("edit-form").addEventListener("submit", async(e) => {
+  e.preventDefault();
+  const question = $("#question").val();
+  const answer = $("#answer").val();
+  console.log(capitalizedString(question))
+  console.log(answer)
+
+  const id = window.location.pathname.split("/")[2];
+  await $.ajax({
+    url: `/api/questions/${id}`,
+    method: "PUT",
+    dataType: "json",
+    data: {
+      question: question,
+      answer: answer
+    }
+  });
+  window.location.pathname = "/admin";
+})
+
+function capitalizedString(string) {
+  if (!string) return "";
+  return string[0].toUpperCase() + string.slice(1, string.length);
+}
+
+document.getElementById("question").addEventListener("input", ({ target })=> {
+  target.value = capitalizedString(target.value);
+})
+
+document.getElementById("answer").addEventListener("input", ({ target })=> {
+  target.value = capitalizedString(target.value);
+})
