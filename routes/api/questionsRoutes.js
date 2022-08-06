6fcc66ecad5c891a6a1cfd9e3d4f7015b28027c9
Joseph Soria <josephsoria6@gmail.com>
2022-08-05 22:39:25 -0500

    Added post method to add questions

--- routes/api/questionsRoutes.js
+++ routes/api/questionsRoutes.js
@@ -1,10 +1,23 @@
 const router = require("express").Router();
-const { restart } = require("nodemon");
 const db = require("../../models"); 
 
 router.get("/", async(req, res) => {
   const questions = await db.Questions.find({});
   res.status(200).json(questions);
+})
+
+router.post("/", async(req,res) => {
+  try {
+    const currentQuestions = await db.Questions.find();
+    await db.Questions.create({
+      questionNumber: String(currentQuestions.length + 1),
+      question: req.body.question,
+      answer: req.body.answer,
+    })
+    res.status(200).json({ message: "Question created!"})
+  } catch(err) {
+    console.log(err.message);
+  }
 })
 
 router.put("/:id", async(req, res) => {
